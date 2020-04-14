47e2ce732ed377c2b18a80cffb769f93a8bbab84
fujiwara <shintaro.fujiwara@gmail.com>
2020-04-14 20:28:17 +0900

    Fix to echo function line, and more debugging strings will be echoed. 	modified:   moodle_debugger.sh

--- moodle_debugger.sh
+++ moodle_debugger.sh
@@ -3,7 +3,7 @@
 ##
 ## This program adds echo line of file name and function name plus sql to related php file of Moodle.
 ##
-## Version 0.1.1
+## Version 0.1.2
 ##
 ## Copyright (C) 2020 Shintaro Fujiwara
 ##
@@ -30,7 +30,18 @@ USER="apache"
 DIR="${HTML_DIR}/moodle_${DEBUG_STR}"
 CONSTRUCTOR="__construct"
 EXCLUDE_FILE="setuplib.php"
-FILELIST_PHP="/tmp/filelist_php"
+FILELIST_PHP="/tmp/moodle_debugger_filelist_php"
+FILELIST_PHP_MULTIPLE="/tmp/moodle_debugger_multiple_debug.log"
+
+if [ -e "${FILELIST_PHP}" ]; then
+    rm -f "${FILELIST_PHP}"
+fi
+
+if [ -e "${FILELIST_PHP}" ]; then
+    rm -f "${FILELIST_PHP_MULTIPLE}"
+fi
+touch "${FILELIST_PHP_MULTIPLE}"
+chmod 0660 "${FILELIST_PHP_MULTIPLE}"
 
 function confirm_question()
 {
@@ -91,7 +102,7 @@ fi
 chmod 0770 "${DIR}"
 echo "Find php files in ${DIR}"
 find "${DIR}" -type f -name "*.php" > "${FILELIST_PHP}"
-trap "echo remove directory ${DIR} and ${FILELIST_PHP};rm -rf ${DIR};rm -f ${FILELIST_PHP};exit 1" 2
+trap "echo remove directory ${DIR} and ${FILELIST_PHP};rm -rf ${DIR};rm -f ${FILELIST_PHP};rm -f ${FILELIST_PHP_MULTIPLE};exit 1" 2
 chmod 660 "${FILELIST_PHP}"
 echo "Foud php files and saved as ${FILELIST_PHP}"
 sleep 3
@@ -100,23 +111,50 @@ sleep 5
 
 while read line
 do
-    if [[ "${line}" =~ "class" ]]; then
-       echo "exclude class file -- ${line}"
-    else
-        grep -e "${CONSTRUCTOR}" "${line}" >/dev/null 2>&1
-        if [ $? == 0 ]; then
-            echo "exclude ${line}, because matched with ${CONSTRUCTOR}"
-        else
-            sed -i -e '/public function [^__].*{/a echo "Called: ".__FILE__." : ".__FUNCTION__."\\n";' "${line}" 2>&1
-        fi
-        grep -e "${rawsql}" "${line}" >/dev/null 2>&1
-        if [ $? == 0 ]; then
-            sed -i -e '/ $rawsql/a print_object($rawsql);' "${line}"  >/dev/null 2>&1
-        fi
+    sed -i -e '/^function [^__].*{/a echo "Called:".__FILE__.":".__FUNCTION__.":".__LINE__."\\n";' "${line}" 2>&1
+    sed -i -e '/^public function [^__].*{/a echo "Called:".__FILE__.":".__FUNCTION__.":".__LINE__."\\n";' "${line}" 2>&1
+    grep -e "${rawsql}" "${line}" >/dev/null 2>&1
+    if [ $? == 0 ]; then
+        sed -i -e '/ $rawsql/a $rawsql_var = print_r($rawsql, true);echo "${rawsql_var}";' "${line}"  >/dev/null 2>&1
     fi
+    echo "${line}"  >> "${FILELIST_PHP_MULTIPLE}" 2>&1
+    awk '(match($0,/^function.*/) && match($0,/,$/)) && /^function/,/.*{$/ {print NR}' "${line}"  >> "${FILELIST_PHP_MULTIPLE}" 2>&1
 done < "${FILELIST_PHP}"
 
+RE='^[0-9]+$'
+LINE_NUMBER_PRE=0
+LINE_NUMBER=0
+LINE_INSERT=0
+SUBTRACTION=0
+INSERT_SUCCESS=0
+while read line
+do
+    if [[ "${line}" =~ $RE ]] ; then
+        LINE_NUMBER_PRE="${LINE_NUMBER}"
+        LINE_NUMBER="${line}"
+        SUBTRACTION=$(echo $((LINE_NUMBER - LINE_NUMBER_PRE))) >/dev/null
+       	if [ "${INSERT_SUCCESS}" -eq 0 ]; then
+       	    if [ "${SUBTRACTION}" -gt 1 ]; then
+                LINE_INSERT=$((LINE_NUMBER + 1))
+                sed -i ${LINE_INSERT}a"echo \"Called:\".__FILE__.\":\".__FUNCTION__.\":\".__LINE__.\"\\\n\";" "${FILE_CANDIDATE_NAME}" 2>&1
+                if [ $? = 0 ]; then
+                    INSERT_SUCCESS=1
+                fi
+            else
+                INSERT_SUCCESS=0
+            fi
+        fi
+    else
+        # file name
+        FILE_CANDIDATE_NAME="${line}"
+        LINE_NUMBER_PRE=0
+        LINE_NUMBER=0
+        INSERT_SUCCESS=0
+    fi
+done < "${FILELIST_PHP_MULTIPLE}"
+
 rm -f "${FILELIST_PHP}"
+rm -f "${FILELIST_PHP_MULTIPLE}"
 
 echo ""
 echo "Now you have ${DIR} to debug your moodle."
